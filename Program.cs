0c39bbad7df70dd9afe7d53287b5a90dac33a738
Nastya Spasskaya <Nastya312217@gmail.com>
2022-09-06 21:13:53 +0700

    Решение задачи 2 к семинару 9

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Показать натуральные числа от М до N, N и М заданны
+
+Console.Clear();
+Console.WriteLine($"Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
